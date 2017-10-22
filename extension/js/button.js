ed7b7ff8ffc68d2d6c1edc5735a67f98591f259f
Oorja Chowdhary <oorjac@uw.edu>
2017-10-22 15:08:27 -0700

    css changes

---
+++ extension/js/button.js
@@ -0,0 +1,104 @@
+   $(document).ready(function(){
+    $('#autismbutton').click(function(){
+    $('#picture').attr('src', '../css/autorangeb.png');
+
+});
+    })
+
+ $(document).ready(function(){
+    $('#screenreaders').click(function(){
+    $('#picture2').attr('src', '../css/srob.png');
+
+});
+    })
+
+ $(document).ready(function(){
+    $('#dyslexia').click(function(){
+    $('#picture3').attr('src', '../css/dysob.png');
+
+});
+    })
+
+ $(document).ready(function(){
+    $('#deaf').click(function(){
+    $('#picture4').attr('src', '../css/deafob.png');
+
+});
+    })
+
+ $(document).ready(function(){
+    $('#physical').click(function(){
+    $('#picture5').attr('src', '../css/physob.png');
+
+});
+    })
+
+$(document).ready(function(){
+    $('#vision').click(function(){
+    $('#picture6').attr('src', '../css/visob.png');
+
+});
+    })
+
+$(document).ready(function(){
+    $('#fonts').click(function(){
+    $('#font1').attr('src', '../css/fonto.png');
+
+});
+    })
+
+$(document).ready(function(){
+    $('#colors').click(function(){
+    $('#color1').attr('src', '../css/brighto.png');
+
+});
+    })
+
+$(document).ready(function(){
+    $('#layout').click(function(){
+    $('#layout1').attr('src', '../css/lefto.png');
+
+});
+    })
+
+$(document).ready(function(){
+    $('#layouts2').click(function(){
+    $('#layout2').attr('src', '../css/lefto.png');
+
+});
+    })
+
+$(document).ready(function(){
+    $('#layouts2').click(function(){
+    $('#layout2').attr('src', '../css/webo.png');
+
+});
+    })
+
+$(document).ready(function(){
+    $('#form').click(function(){
+    $('#form1').attr('src', '../css/buttono.png');
+
+});
+    })
+
+$(document).ready(function(){
+    $('#forms2').click(function(){
+    $('#form2').attr('src', '../css/formo.png');
+
+});
+    })
+
+$(document).ready(function(){
+    $('#language').click(function(){
+    $('#language1').attr('src', '../css/webo.png');
+
+});
+    })
+
+$(document).ready(function(){
+    $('#language').click(function(){
+    $('#language1').attr('src', '../css/sumo.png');
+
+});
+    })
